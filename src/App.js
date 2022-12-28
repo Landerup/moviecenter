1bcf3680c6c2ba0391d0bb271b016d370ac3993d
RasmusLanderup <Rasmus.landerup@live.se>
2022-12-28 16:39:16 +0100

    add searchbar and function for searching plus some average styling

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import MovieHeader from "./components/MovieHeader";
 function App() {
   const API_URL = "https://api.themoviedb.org/3";
   const IMG_URL = "https://image.tmdb.org/t/p/w300";
+  const API_SEARCH = "https://api.themoviedb.org/3/search/movie"
   const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
 
-  // Lägger till api url samt api key i en fetch med axios
-  const fetchMovieApi = async (searchMovie) => {
+  // Lägger till api url samt api key i en fetch
+  const fetchMovieApi = async () => {
     const apiData = await fetch(`${API_URL}/discover/movie?api_key=${API_KEY}`);
     const apiDataJson = await apiData.json();
 
     setMovies(apiDataJson.results);
-
   };
 
   // Kallar på fetch funktionen, kallas bara när sidan laddas, sidan laddas om vid ny sökning
@@ -30,16 +31,57 @@ function App() {
       <MovieCard key={movie.id} movie={movie} img_url={IMG_URL} />
     ));
 
+  // lägger in sök url och fetchar den
+  const searchMovie = async(event) => {
+    event.preventDefault();
+    console.log("searching")
+    try{
+      const search_url = `${API_SEARCH}?api_key=${API_KEY}&query=${search}`;
+      const searchData = await fetch(search_url);
+      const searchDataJson = await searchData.json();
+
+      setMovies(searchDataJson.results);
+    }
+    catch(event){
+
+    }
+  }
+
+  const seachHandler= (event)=> {
+    setSearch(event.target.value);
+  }
+
   // Listar filmer på hemsidan med listMovies funktionen
   return (
-      <div className="container-fluid movie-app">
-        <div className="row d-flex align-items-center mt-4 mb-4 m-1">
-          <MovieHeader header="Movie Center"/>
+    <div className="container-fluid">
+      <header className="header">
+        <div className="m-4">
+          <MovieHeader header="Movie Center" />
         </div>
-        <div className="row">
-          <div className="d-flex justify-content-start">{listMovies()}</div>
+      </header>
+      <main>
+        <div className="m-4">
+          <form className="col-sm-4" onSubmit={searchMovie}>
+            <input
+              className="form-control form-control-sm"
+              type="search"
+              placeholder="Search here..."
+              name="query"
+              value={search}
+              onChange={seachHandler}
+            />
+            <button type="submit" className="btn btn-outline-light btn-sm">
+              Search
+            </button>
+          </form>
         </div>
-      </div>
+        <div className="text-center m-4">
+          <h3>New Movies</h3>
+        </div>
+        <div className="row justify-content-center">{listMovies()}</div>
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,9 +3,10 @@ import React from "react";
 // Väljer vad jag vill hämta från film data, renderar film data här
 const MovieCard = ({ movie, img_url }) => {
   return (
-      <div className="m-2">
+    // kollar om filmen har en poster isåfall ska den visas annars inte
+      <div className="col col-3 m-2">
         <div>
-          <img src={`${img_url}${movie.poster_path}`} alt="movie"/>
+        {movie.poster_path ? <img src={`${img_url}${movie.poster_path}`} alt="movie"/> : null}
         </div>
       </div>
   );
