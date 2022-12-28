dd62d624efc63c922213bbe785053549bac4f501
RasmusLanderup <Rasmus.landerup@live.se>
2022-12-28 12:50:37 +0100

    add api and output data from api

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieCard from './components/MovieCard';
 
 function App() {
 
   const API_URL = "https://api.themoviedb.org/3";
+  const IMG_URL = "https://image.tmdb.org/t/p/w300"
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
   const [movies, setMovies] = useState([]);
 
   // Lägger till api url samt api key i en fetch med axios
   const fetchMovieApi = async () => {
-    const {apiData: results} = await axios.get(`${API_URL}/discover/movie`, {
-      params: {
-        api_key: process.env.REACT_APP_MOVIE_API_KEY
-      }
-    })
+    const apiData = await fetch(`${API_URL}/discover/movie?api_key=${API_KEY}`);
+    const apiDataJson = await apiData.json();
 
-  setMovies(results)
+    console.log(apiDataJson)
+
+    setMovies(apiDataJson.results);
+
+    console.log(movies)
   };
 
-  // Kallar på fetch funktionen
+  // Kallar på fetch funktionen, kallas bara när sidan laddas
   useEffect(() => {
     fetchMovieApi();
-  },[]);
+  }, []);
 
   // Funktion för att hämta data från MovieCard komponenten, vad jag vill lista ut
   const listMovies = () => (
-    movies.map(movie => (
+    movies.map((movie =>
       <MovieCard
-        key={movie.id}
-        movie={movie}
+      key = {movie.id}
+      movie = {movie}
+      img_url = {IMG_URL}
       />
-    ))
-  )
+      ))
+  );
 
   // Listar filmer på hemsidan med listMovies funktionen
   return (
-    <div className="App">
-      <h1>Hej bror!</h1>
-      <div className='container'>
-        {listMovies()}
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+          <div  className="d-flex justify-content-start m-3">
+            {listMovies()}
+          </div>
       </div>
     </div>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 // Väljer vad jag vill hämta från film datan
-const MovieCard = ({movie}) => {
-    return (
-        <>
-            <div>
-                {movie.title}
-            </div>
-        </>
-    );
+const MovieCard = ({ movie, img_url }) => {
+  return (
+      <div>
+        <div>
+          <h5>{movie.title}</h5>
+          <img src={`${img_url}${movie.poster_path}`} alt="movie" />
+        </div>
+      </div>
+  );
 };
 
 export default MovieCard;
