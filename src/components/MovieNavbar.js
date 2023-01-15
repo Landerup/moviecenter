290c5c708f7cdffbbb3ed82eca3c470d8e28b826
RasmusLanderup <Rasmus.landerup@live.se>
2023-01-15 13:41:57 +0100

    add Links to navbar

--- src/components/MovieNavbar.js
+++ src/components/MovieNavbar.js
@@ -5,6 +5,7 @@ import MovieUserProfile from "./MovieUserProfile";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 // if-sats för att kolla om man är inloggad eller inte, för att visa login eller logout knapp
 // använder react-bootstrap för enklare responsivitet och burger-menu
@@ -13,20 +14,20 @@ const MovieNavbar = () => {
 
   return (
     <>
-      <Navbar expand="md" variant="dark">
+      <Navbar expand="md" variant="dark" className="m-2">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto gap-4 m-4">
-              <Nav.Link className="text-white" href="/new">
+            <Nav className="me-auto gap-5 m-4">
+              <Link className="text-white" to="/new">
                 New Movies
-              </Nav.Link>
-              <Nav.Link className="text-white" href="/popular">
+              </Link>
+              <Link className="text-white" to="/popular">
                 Popular Movies
-              </Nav.Link>
-              <Nav.Link className="text-white" href="/profile">
+              </Link>
+              <Link className="text-white" to="/profile">
                 <MovieUserProfile />
-              </Nav.Link>
+              </Link>
               {isAuthenticated ? <MovieLogout /> : <MovieLogin />}
             </Nav>
           </Navbar.Collapse>
